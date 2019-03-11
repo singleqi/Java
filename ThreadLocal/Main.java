51fedd02b21e63946ca1e9c56d21715bde8fe894
singleqi <18888106773@163.com>
2019-03-11 20:57:39 +0800

    ThreadLocal弱引用内存泄漏实例

--- ThreadLocal/Main.java
+++ ThreadLocal/Main.java
@@ -1,24 +1,25 @@
 package cn.singleqi;
 
+import java.lang.ref.WeakReference;
+import java.util.Map;
+
 /**
  * ThreadLocal的使用、弱引用、内存泄漏
  * https://www.jianshu.com/p/98b68c97df9b
  * https://www.jianshu.com/p/a1cd61fa22da
  */
 public class Main {
-    private static ThreadLocal<Integer> threadLocal = new ThreadLocal<>();
 
     public static void main(String[] args) {
+        ThreadLocal<String> threadLocal = new ThreadLocal<>();
         //在main线程中设置threadLocal值
-        threadLocal.set(111);
-        Integer i = threadLocal.get();
-        System.out.println(Thread.currentThread().getName() + "\t" + i);
+        threadLocal.set("threadLocal main thread set");
+        System.out.println(Thread.currentThread().getName() + "\t" + threadLocal.get());
         new Thread(() ->
         {
             //在Thread-0中设置threadLocal值
-            threadLocal.set(222);
-            Integer t = threadLocal.get();
-            System.out.println(Thread.currentThread().getName() + "\t" + t);
+            threadLocal.set("threadLocal other thread set");
+            System.out.println(Thread.currentThread().getName() + "\t" + threadLocal.get());
         }
         ).start();
         //等待Thread-0中的值设置threadLocal，并输出
@@ -28,26 +29,40 @@ public class Main {
             e.printStackTrace();
         }
         //再次获取main线程中的threadLocal值
-        i = threadLocal.get();
-        System.out.println(Thread.currentThread().getName() + "\t" + i);
-        new Thread(() ->
-        {
-            //清除threadLocal的在虚拟机中的强引用
-            threadLocal = null;
-            //强制GC
-            System.gc();
-            //此时ThreadLocalMap中的Entry的key的弱应用已被清除
-            Integer t = threadLocal.get();
-            System.out.println(Thread.currentThread().getName() + "\t" + t);
-        }
-        ).start();
+        System.out.println(Thread.currentThread().getName() + "\t" + threadLocal.get());
+        System.out.println();
+
+
+        //ThreadLocalMap的key弱引用ThreadLocal在gc时被回收，value没有被回收，导致内存泄漏
+        //但如果是强引用的话，线程不死，ThreadLocalMap的key-value一直在涨
+        ThreadLocal tl = new MyThreadLocal();
+        tl.set(new My50MB());
+        tl = null;
+        System.out.println("Full GC");
+        System.gc();
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        //Main线程中的ThreadLocalMap中的Entry的key的弱应用也已被清除
-        i = threadLocal.get();
-        System.out.println(Thread.currentThread().getName() + "\t" + i);
+        System.out.println("After Full GC");
+    }
+
+    public static class MyThreadLocal extends ThreadLocal {
+        private byte[] a = new byte[1024 * 1024 * 1];
+
+        @Override
+        public void finalize() {
+            System.out.println("My threadlocal 1 MB finalized.");
+        }
+    }
+
+    public static class My50MB {
+        private byte[] a = new byte[1024 * 1024 * 50];
+
+        @Override
+        public void finalize() {
+            System.out.println("My 50 MB finalized.");
+        }
     }
 }
